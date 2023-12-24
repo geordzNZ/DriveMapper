1810cebb00de7456d4f30a9d57a45562ff0d0f91
GeordzNZ <geordie.king@gmail.com>
2023-12-24 01:27:37 +0000

    first pass at outputting folders/files

--- DriveMapper/Program.cs
+++ DriveMapper/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace DriveMapper
+﻿using System.IO;
+
+namespace DriveMapper
 {
 
 
@@ -6,7 +8,37 @@
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Listing contents of C Drive...");
+                Console.WriteLine($"\tDIRECTORIES");
+                foreach (var dir in Directory.EnumerateDirectories(@"C:\"))
+                {
+                    Console.WriteLine($"\t\t{dir}");
+                }
+                Console.WriteLine($"\tFILES");
+                //foreach (var file in Directory.GetFiles(@"C:\"))
+                foreach (var file in Directory.GetFiles(@"C:\"))
+                {
+                    Console.WriteLine($"\t\t{file}");
+                }
+            }
+            else
+            {
+                string currDir = Directory.GetCurrentDirectory();
 
+                Console.WriteLine("Listing contents of current directory...");
+                Console.WriteLine($"\tDIRECTORIES");
+                foreach (var dir in Directory.EnumerateDirectories(currDir))
+                {
+                    Console.WriteLine($"\t\t{dir}");
+                }
+                Console.WriteLine($"\tFILES");
+                foreach (var file in Directory.GetFiles(currDir))
+                {
+                    Console.WriteLine($"\t\t{file}");
+                }
+            }
 
         }
     }
