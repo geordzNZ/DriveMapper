172293487821ea798c540f2bca85b322fb380b6a
GeordzNZ <geordie.king@gmail.com>
2023-12-27 22:54:48 +0000

    Added file logging code for top level folder

--- DriveMapper/Program.cs
+++ DriveMapper/Program.cs
@@ -11,6 +11,13 @@ namespace DriveMapper
             if (args.Length == 0)
             {
                 Console.WriteLine("Listing contents of C Drive...");
+
+                Console.WriteLine($"\t\t{@"C:\"}");
+                foreach (var file in Directory.GetFiles(@"C:\"))
+                {
+                    Console.WriteLine($"\t\t\t{file}");
+                }
+
                 foreach (var dir in Directory.EnumerateDirectories(@"C:\"))
                 {
                     Console.WriteLine($"\t\t{dir}");
@@ -25,6 +32,12 @@ namespace DriveMapper
                 string currDir = Directory.GetCurrentDirectory();
 
                 Console.WriteLine("Listing contents of current directory...");
+                Console.WriteLine($"\t\t{currDir}");
+                foreach (var file in Directory.GetFiles(currDir))
+                {
+                    Console.WriteLine($"\t\t\t{file}");
+                }
+
                 foreach (var dir in Directory.EnumerateDirectories(currDir))
                 {
                     Console.WriteLine($"\t\t{dir}");
